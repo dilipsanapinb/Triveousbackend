f7835676c6b579702a7dbcba5b533a25d31cb309
DILIP SANAP <dilipinbsanap@gmail.com>
2023-08-31 22:31:04 +0530

    connected to DB and user model is designed

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-
+const connection=require('./config/db')
 const port=process.env.PORT || 5000
 const app = express();
 
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: "Welcome to the triveous app" })
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    try {
+        await connection;
+        console.log('Connected to DB');
+    } catch (error) {
+        console.log('Something went wrong at connecting the DB');
+    }
     console.log(`Server is running on port: ${port}`);
 })
